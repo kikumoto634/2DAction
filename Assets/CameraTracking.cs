0fa45eb283b029264b830b6219ce17394e93a687
634_3 <k020g1146@g.neec.ac.jp>
2022-05-04 23:54:40 +0900

    動くオブジェクト実装

---
+++ Assets/CameraTracking.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    private GameObject Player = null;
+    private Transform PlayerTransform = default;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerTransform = Player.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(PlayerTransform.position.x, transform.position.y, transform.position.z);
+    }
+}
